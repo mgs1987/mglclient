7e8907012e462009b9c2c5bf35a9a55672de685b
Mek <mgdesampaio@gmail.com>
2024-04-04 19:46:37 -0400

    estiloytraducciones

--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import i18nBackend from "i18next-http-backend";
 const getCurrentHost =
-  import.meta.env.MODE === "development"
+  import.meta.env.VITE_MODE === "development"
     ? "http://localhost:5173"
     : "https://gorgeous-sunflower-93ea3b.netlify.app/";
 i18n
--- src/trajectory/Trajectory.jsx
+++ src/trajectory/Trajectory.jsx
@@ -2,8 +2,8 @@ import { useTranslation } from "react-i18next";
 const Trajectory = () => {
   const { t } = useTranslation();
   return (
-    <div className="md:px-28 md:py-40 s:px-16 s:py-32 xs:px-12 xs:py-28 lg:py-40 flex flex-col justify-center items-center bg-base text-darkBlue font-title">
-      <h1 className="font-semibold text-5xl text-center">
+    <div className="md:px-28 md:py-40 s:px-16 s:py-32 xs:px-12 xs:py-28 lg:pt-40 lg:pb-20 flex flex-col justify-center items-center bg-base text-darkBlue font-title">
+      <h1 className="font-semibold text-5xl text-center lg:pb-[3.75rem] lg:mb-0">
         {t("ourtrajectory")}
       </h1>
       <section className="flex-row text-center tracking-wide lg:text-lg xs:text-lg s:text-lg md:text-2xl">
