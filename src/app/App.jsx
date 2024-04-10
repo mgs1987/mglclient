ddef99c4b9bab1ba2843b0f59d2b5bcb28b51584
Mek <mgdesampaio@gmail.com>
2024-04-10 13:19:17 -0400

    margin footer img linkedin

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,7 +6,6 @@ import Team from "../team/Team";
 import Wrapper from "../wrapper/Wrapper";
 import { Routes, Route } from "react-router-dom";
 import Contactus from "../contactus/Contactus";
-import Form from "../Form/Form";
 import "../i18n";
 import Login from "../Login/Login";
 import DashBoard from "./dashBoard/DashBoard";
@@ -23,12 +22,11 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Wrapper />}>
             <Route index element={<Home />} />
-            <Route exact path="/team" element={<Team />} />
-            <Route exact path="/contactus" element={<Contactus />} />
-            <Route exact path="/areas" element={<Areas />} />
-            <Route exact path="/home/login" element={<Login />} />
-            <Route exact path="/login/dashboard" element={<DashBoard />} />
-            <Route exact path="/login/form" element={<Form />} />
+            <Route exact path="team" element={<Team />} />
+            <Route exact path="contactus" element={<Contactus />} />
+            <Route exact path="areas" element={<Areas />} />
+            <Route exact path="login" element={<Login />} />
+            <Route exact path="login/dashboard" element={<DashBoard />} />
             <Route path="*" element={<Error />} />
           </Route>
         </Routes>
