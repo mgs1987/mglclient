6e685b0aea274639c630706f1cdbebd17d4262a9
Mek <mgdesampaio@gmail.com>
2024-04-12 12:08:16 -0400

    traduccion

--- src/i18n.js
+++ src/i18n.js
@@ -4,7 +4,8 @@ import i18nBackend from "i18next-http-backend";
 const getCurrentHost =
   import.meta.env.VITE_MODE === "development"
     ? "http://localhost:5173"
-    : "https://gorgeous-sunflower-93ea3b.netlify.app/";
+    : "https://gorgeous-sunflower-93ea3b.netlify.app/" ||
+      "https://mglabogados.com/";
 i18n
   .use(i18nBackend)
   .use(initReactI18next)
